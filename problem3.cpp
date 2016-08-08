5c74a5681cb91ef1d0af2af1c9c36934b99a2e4c
alvin562 <alvindantic562@gmail.com>
2016-08-07 23:27:32 -0700

    finished solution to problem 3

--- problem3.cpp
+++ problem3.cpp
@@ -4,7 +4,7 @@
 
 
 // returns true if number is prime; false otherwise
-int isPrime(int n)
+int isPrime(long n)
 {
     if(n <= 1)
         
@@ -21,15 +21,43 @@ int isPrime(int n)
 }
 
 
+//return the largest prime factor
+long largestPrimeFactor(long n)
+{
+    if(isPrime(n))
+        
+        return n;
+    
+    
+    for(int i = 2; i <= n/2; i++) {
+        
+        
+        //we enter this if statement for
+        //any integer greater than 1 that
+        //isn't already prime
+        if(n % i == 0) {
+            
+            //we find the largest prime factor
+            //from the left and right sub trees
+            //of n's prime factorization tree
+            long n1 = largestPrimeFactor(n/i);
+            long n2 = largestPrimeFactor(i);
+            
+            //the largest prime factor is
+            //the largest of the two
+            return (n1 > n2) ? n1 : n2;
+        }
+    }
+    
+    //we should never get here
+    return -1;
+}
+
+
 int main(int argc, const char * argv[])
 {
-    for(int i = 1; i <= 30; i++) {
-        
-        if(isPrime(i))
-            
-            std::cout << i << " is prime" << std::endl;
-        else
-            
-            std::cout << i << " is not prime" << std::endl;
-    }
+    long answer = largestPrimeFactor(600851475143);
+    
+    std::cout << "Project Euler - Problem 3: " << answer << std::endl;
+    return 0;
 }
